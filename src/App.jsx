12ba6c3fa80abaaffc46e9c397f09e920e73fdcd
Jessica Parrish <JWal951@wgu.edu>
2025-05-03 03:09:26 -0400

    Added images and new web page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import Footer from './components/Footer';
+import ResultPage from './pages/RecipeResultPage';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <ResultPage />
     </div>
   );
 }
